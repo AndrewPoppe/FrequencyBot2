21c1ca1d6092d262821086337df88817bea52f46
AndrewPoppe <AndrewBPoppe@gmail.com>
2019-05-15 22:39:31 -0400

    continuing the rewrite

--- src/js/page_action.js
+++ src/js/page_action.js
@@ -1,5 +1,43 @@
 
 
+// function to take scale name and return object with type and semitones
+function getScale(name) {
+	let result = {},
+		scale = Tonal.scale(name);
+	if (scale === undefined) {
+		//TODO
+	} else {
+		semitones = scale.map(Tonal.Interval.semitones);
+		result.type = name;
+		result.semitones = semitones;
+	}
+	return result;
+}
+
+// Get last used scale to set that as default. Then add in all scale options to
+// scale type selector
+chrome.storage.local.get('lastScaleType', res => {
+	if (res.type === undefined) res = getScale("major");
+	Tonal.Scale.names().map(name => {
+		let selected = "";
+		if (name === res.type) selected = " selected";
+	    $('#ScaleTypeSelect').append('<option value="'+name+'"'+selected+'>'+name+'</option>');
+	});
+	$('#DefineScaleInput').val(res.semitones.toString());
+})
+
+// Callback for when a scale is selected in scale type selector
+$('#ScaleTypeSelect').on('change', e => {
+	let newScaleName = e.target.value,
+		newScale = getScale(newScaleName);
+	chrome.storage.local.set({selectedScaleType: newScale}, res => {
+		$('#DefineScaleInput').val(newScale.semitones.toString());
+	});
+});
+
+
+
+
 let allFiles = {};
 let unmatchedFiles = {raw: [], ren: []};
 let MaxRam;
